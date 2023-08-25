52421969d64989044740590b0397c382144aa540
mutembeic <135004069+mutembeic@users.noreply.github.com>
2023-08-25 08:13:22 +0300

    done

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,24 @@
 def badge_maker(name):
-    return None
-
+      return  (f"Hello, my name is {name}.")
+     
 def batch_badge_creator(names):
-    return None
+    badge_messages = []
+    for name in names:
+        badge_messages.append(f"Hello, my name is {name}.")
+    return badge_messages
 
 def assign_rooms(names):
-    return None
-
+    room_assignments = []
+    for index, name in enumerate(names, start=1):
+        room_assignments.append(f"Hello, {name}! You'll be assigned to room {index}!")
+    return room_assignments
+                  
 def printer(names):
-    return None
+    badge_messages = batch_badge_creator(names)
+    room_assignments = assign_rooms(names)
+
+    for message in badge_messages:
+        print(message)
+
+    for assignment in room_assignments:
+        print(assignment)
